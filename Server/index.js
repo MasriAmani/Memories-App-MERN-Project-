71bee97d2bc343f7adfa510f7b130321e7cb566b
Mohamad <amanimasri92@gmail.com>
2022-05-24 02:01:10 +0300

    Connecting to the database

--- Server/index.js
+++ Server/index.js
@@ -4,3 +4,17 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 
 
+const app = express();
+
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://amani92:amani1234@cluster0.a1ipl.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL,{useNewUrlParser: true, useUnifiedTopology:true})
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+//mongoose.set('useFindAndModify', false);
