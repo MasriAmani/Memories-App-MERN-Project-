d8d4e109325a7b6895a0c07350b6f1fe675b18d4
Mohamad <amanimasri92@gmail.com>
2022-06-06 00:56:04 +0300

    Dispatching getPosts action

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React , {useEffect} from 'react';
 import { Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
+import { useDispatch } from 'react-redux';
+import { getPosts } from './actions/posts';
 
 import Posts from "./components/Posts/Posts";
 import Form from "./components/Form/Form";
@@ -8,6 +10,14 @@ import memories from './images/memories.png';
 
 const App = () => {
     const classes = useStyles();
+    const dispatch = useDispatch();
+
+    useEffect (() => {
+      
+        dispatch(getPosts());
+
+    }, [dispatch]);
+
     return(
         
         <Container maxidth ="lg">
