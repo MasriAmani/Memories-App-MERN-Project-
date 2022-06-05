4bf266e170209542b7b7f17db6a60305bc50c9b5
Mohamad <amanimasri92@gmail.com>
2022-06-06 01:02:44 +0300

    Added prefix for the post routes

--- Server/index.js
+++ Server/index.js
@@ -3,12 +3,16 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoutes from './routes/posts.js';
+
 
 const app = express();
+
 
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
+app.use('/posts',postRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://amani92:amani1234@cluster0.a1ipl.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
@@ -17,4 +21,3 @@ mongoose.connect(CONNECTION_URL,{useNewUrlParser: true, useUnifiedTopology:true}
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
-//mongoose.set('useFindAndModify', false);
